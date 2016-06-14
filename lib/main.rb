b36e3881092178b40e127da925b94d374bc7a3d7
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-06-14 18:06:03 +0900

    本番と同様に標準入出力を引数に設定

--- lib/main.rb
+++ lib/main.rb
@@ -1,14 +1,12 @@
-print 'メニューを選んでください。'
+require 'menu'
+
+puts 'メニューを選んでください。'
 
 while true do
   number = gets.chomp
 
   break if number == '0'
 
-  if number == '1'
-    print 'FizzBuzz実行'
-  elsif number == '2'
-    print '履歴表示'
-  end
+  Menu.new(STDIN, STDOUT).select(number)
 
 end
