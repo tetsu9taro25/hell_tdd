aed017bd98a85e84faeba48d34bb6cc609161625
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-06-16 15:14:55 +0900

    整数のテストと、英字・記号のテスト

--- spec/integer_validator_spec.rb
+++ spec/integer_validator_spec.rb
@@ -1,7 +1,22 @@
 require 'integer_validator'
 describe IntegerValidator do
   it do
+    obj = IntegerValidator.new('3')
+    expect(obj.valid?).to be_truthy
+  end
+
+  it do
     obj = IntegerValidator.new('0.2')
+    expect(obj.valid?).to be_falsy
+  end
+
+  it do
+    obj = IntegerValidator.new('a')
+    expect(obj.valid?).to be_falsy
+  end
+
+  it do
+    obj = IntegerValidator.new('√')
     expect(obj.valid?).to be_falsy
   end
 end
