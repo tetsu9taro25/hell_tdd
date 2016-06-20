d67c08bee6f3b0541d7e7e00581caeff08a90f88
Tetsutaro Endo <tetsutaro.endo@gaiax.com>
2016-06-20 16:34:12 +0900

    メニューテストのリファクタリング

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -22,25 +22,36 @@ end
 
 describe Menu do
   let(:spy){ StdoutSpy.new }
+  let(:stub){ StdinStub.new(input) }
+  let(:menu){ described_class.new(stub, spy) }
 
-  it do
-    stub = StdinStub.new('3')
-    obj = Menu.new(stub, spy)
-    obj.select('1')
-    expect(spy.result).to eq('Fizz')
+  context '1を選択する場合' do
+    context '正常系' do
+      let(:input){ '3' }
+
+      it do
+        menu.select('1')
+        expect(spy.result).to eq('Fizz')
+      end
+    end
+
+    context '異常系' do
+      let(:input){ '0.2' }
+
+      it do
+        menu.select('1')
+        expect(spy.result).to eq('整数を入力してください')
+      end
+    end
   end
 
-  it do
-    stub = StdinStub.new('0.2')
-    obj = Menu.new(stub, spy)
-    obj.select('1')
-    expect(spy.result).to eq('整数を入力してください')
-  end
+  context '存在しないメニューを選択する場合' do
+    let(:input){ nil }
 
-  it do
-    obj = Menu.new(nil, spy)
-    obj.select('100')
-    expect(spy.result).to be_nil
+    it do
+      menu.select('100')
+      expect(spy.result).to be_nil
+    end
   end
 
   # spyを本番で使うSTDOUTにしてテストした例
